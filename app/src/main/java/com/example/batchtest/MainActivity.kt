02d054ce418cf682b2962e5017a22d46b886384a
Hagop100 <htanashian@gmail.com>
2023-01-26 15:57:35 -0800

    Hagop made a change

--- app/src/main/java/com/example/batchtest/MainActivity.kt
+++ app/src/main/java/com/example/batchtest/MainActivity.kt
@@ -7,6 +7,8 @@ import com.example.batchtest.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+//Hagop has made a change!
+
 private const val TAG = "NewUserLog";
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding;
