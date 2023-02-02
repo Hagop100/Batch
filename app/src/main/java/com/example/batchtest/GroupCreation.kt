9ad31d8ad3fba0fecb01f7c0ecbfc75c419bed05
Victoria Tran <123702307+thaiteaa@users.noreply.github.com>
2023-02-01 20:46:16 -0800

    idk what happened but im pushing

--- app/src/main/java/com/example/batchtest/GroupCreation.kt
+++ app/src/main/java/com/example/batchtest/GroupCreation.kt
@@ -5,7 +5,7 @@ import java.util.*
 data class GroupCreation (
     val groupCode: UUID,
     val groupName: String,
-    val tags: String,
+//    val tags: String, List of string
     val groupDescription: String
 
 )
--- app/src/main/java/com/example/batchtest/MyGroupFragment.kt
+++ app/src/main/java/com/example/batchtest/MyGroupFragment.kt
@@ -5,15 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import com.example.batchtest.databinding.FragmentGroupCreationBinding
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
-import org.w3c.dom.Text
-import java.security.acl.Group
-import java.util.*
+import androidx.navigation.fragment.findNavController
+import com.example.batchtest.databinding.FragmentMyGroupBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,42 +15,37 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GroupCreationFragment.newInstance] factory method to
+ * Use the [MyGroupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GroupCreationFragment : Fragment() {
+class MyGroupFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private lateinit var binding: FragmentGroupCreationBinding
-    private lateinit var groupCreation: GroupCreation
+    private var param1: String? = null
+    private var param2: String? = null
 
+    private lateinit var binding : FragmentMyGroupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        initialize the values of  GroupCreation class
-        groupCreation = GroupCreation(
-            groupCode = UUID.randomUUID(),
-            groupName = "",
-//            tags = "",
-            groupDescription = ""
-        )
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentGroupCreationBinding.inflate(layoutInflater, container, false)
+        binding = FragmentMyGroupBinding.inflate(layoutInflater,container, false)
 
+        binding.btnToGroupCreation.setOnClickListener{
+            findNavController().navigate(R.id.to_groupCreationFragment)
+        }
         return binding.root
+
+
     }
-
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//    }
 
     companion object {
         /**
@@ -66,12 +54,12 @@ class GroupCreationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment GroupCreationFragment.
+         * @return A new instance of fragment MyGroupFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GroupCreationFragment().apply {
+            MyGroupFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
