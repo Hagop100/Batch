8d59327b67f4ebea6ae2282df4e9be19c9339b51
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-02-12 18:13:21 -0800

    Emanuel Update: Added more values to the User class, Improved my initial profile fragment
    Added FireStore Storage implementation to gradle

--- app/src/main/java/com/example/batchtest/User.kt
+++ app/src/main/java/com/example/batchtest/User.kt
@@ -1,5 +1,7 @@
 package com.example.batchtest
 
+Addimport android.icu.number.NumberFormatter.DecimalSeparatorDisplay
+
 // User data class
 // @param firstName - first name of user
 // @param lastName - last name of user
@@ -9,6 +11,12 @@ data class User(
     val firstName: String? = null,
     val lastName: String? = null,
     val email: String? = null,
+    val displayName : String = "",
+    val gender: String = "N/A",
+    val imageUrl: String? = null,
+    val birthdate: String ="",
+    val personalBio: String = "",
+
     // password ???
 ) {
     // returns full name of user
