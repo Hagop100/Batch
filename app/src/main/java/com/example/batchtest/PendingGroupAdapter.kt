fe37be5f44cfce963adeb45108da1f9e89072dfd
stevennguyens <stevennguyyenz@gmail.com>
2023-03-01 18:01:19 -0800

    added vote cards to pending tab using recycler view and adapters

---
+++ app/src/main/java/com/example/batchtest/PendingGroupAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.batchtest
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import android.content.Context
+import com.example.batchtest.databinding.VoteGroupCardBinding
+import de.hdodenhof.circleimageview.CircleImageView
+
+class PendingGroupAdapter(private val context: Context?, private val groups: ArrayList<Group>) : RecyclerView.Adapter<PendingGroupAdapter.PendingGroupHolder>() {
+    // holder class for each pending group
+    class PendingGroupHolder(val binding: VoteGroupCardBinding) : RecyclerView.ViewHolder(binding.root) {
+        // get the views of card
+        // name of pending group
+        val pendingGroupName: TextView = binding.pendingGroupName
+        // profile picture of user's group that initiated voting
+        val myGroupImg: CircleImageView = binding.myGroupImg
+        // profile picture of pending group
+        val pendingGroupImg: CircleImageView = binding.pendingGroupImg
+    }
+
+    // inflate parent fragment with card item layout when ViewHolder is created
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingGroupHolder {
+        // inflater from parent fragment
+        val inflater = LayoutInflater.from(parent.context)
+
+        // inflate parent using vote group card layout
+        val binding = VoteGroupCardBinding.inflate(inflater, parent, false)
+
+
+        // pass binding into the holder
+        return PendingGroupHolder(binding)
+    }
+
+    // set the group information for each pending group that will be displayed
+    override fun onBindViewHolder(holder: PendingGroupHolder, position: Int) {
+        val group = groups[position]
+        holder.pendingGroupName.text = group.name
+//        if (context != null) {
+//            Glide.with(context).load(group.image).into(holder.pendingGroupImg)
+//        }
+
+    }
+
+    // returns number of pending groups
+    override fun getItemCount(): Int {
+        return groups.size
+    }
+
+}
--- app/src/main/java/com/example/batchtest/PendingGroupFragment.kt
+++ app/src/main/java/com/example/batchtest/PendingGroupFragment.kt
@@ -1,21 +1,72 @@
 package com.example.batchtest
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.batchtest.databinding.FragmentPendingGroupBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+private const val TAG = "PendingGroupsLog"
 /**
  * A simple [Fragment] subclass.
  * Use the [PendingGroupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class PendingGroupFragment : Fragment() {
+    private lateinit var binding: FragmentPendingGroupBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pending_group, container, false)
+        // inflate the layout for pending group fragment
+        binding = FragmentPendingGroupBinding.inflate(inflater, container, false)
+
+        // get recycler view from pending group fragment layout binding
+        val pendingGroupRV = binding.pendingGroupRecyclerView
+        // use grid layout for the recycler view with 2 columns
+        val layoutManager = GridLayoutManager(context, 2)
+        pendingGroupRV.layoutManager = layoutManager
+        // create the default divider used in recycler views in order to remove
+        val dividerItemDecoration = DividerItemDecoration(
+            pendingGroupRV.context,
+            layoutManager.orientation
+        )
+        // remove the default divider from the recycler view
+        pendingGroupRV.removeItemDecoration(dividerItemDecoration)
+
+
+        // fetch groups from database using firebase's firestore
+        val pendingGroups = arrayListOf<Group>()
+        val db = Firebase.firestore
+        /*
+        * fetch all groups and send to adapter which
+        * will display the groups in a recycler view
+         */
+        db.collection("groups")
+            .get()
+            .addOnSuccessListener { result ->
+                for (doc in result) {
+                    // get group's data in form of map
+                    val group: Group = doc.toObject(Group::class.java)
+                    // add group to groups
+                    pendingGroups.add(group)
+                }
+                // attach adapter and send groups
+                val pendingGroupAdapter = PendingGroupAdapter(context, pendingGroups)
+                pendingGroupRV.adapter = pendingGroupAdapter
+            }
+            .addOnFailureListener { e ->
+                Log.v(TAG, "error getting documents: ", e)
+            }
+
+
+        return binding.root
     }
 }
