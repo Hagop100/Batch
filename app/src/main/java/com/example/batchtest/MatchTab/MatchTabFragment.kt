f0020f348fae68043c83022f83ca85a1176da598
stevennguyens <stevennguyyenz@gmail.com>
2023-03-02 19:13:21 -0800

    created a current user doc ref variable

--- app/src/main/java/com/example/batchtest/MatchTab/MatchTabFragment.kt
+++ app/src/main/java/com/example/batchtest/MatchTab/MatchTabFragment.kt
@@ -12,6 +12,7 @@ import com.example.batchtest.MainActivity
 import com.example.batchtest.User
 import com.example.batchtest.databinding.FragmentMatchTabBinding
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.*
@@ -44,6 +45,50 @@ class MatchTabFragment : Fragment(), CardStackAdapter.CardStackAdapterListener {
         // show one card on stack
         manager.setStackFrom(StackFrom.None)
 
+        // manager will define the layout for the card stack view
+        cardStackView.layoutManager = manager
+
+        /*
+        * fetch all possible groups to match based on logged in user
+        * and send to adapter which will display the groups in a recycler view
+         */
+        val groups = arrayListOf<Group>()
+        val db = Firebase.firestore
+        // get the authenticated logged in user
+        val currentUser = Firebase.auth.currentUser
+        // fetches a user from firestore using the uid from the authenticated user
+        val currentUserDocRef = db.collection("users").document(currentUser!!.uid)
+        currentUserDocRef
+            // reads the document reference
+            .get()
+            // if successful filter out certain groups for matching
+            .addOnSuccessListener { result ->
+                // convert the fetched user into a User object
+                val user: User = result.toObject(User::class.java)!!
+                // filter groups will store all group to remove from the match group pool
+                val filterGroups: ArrayList<String> = ArrayList()
+                // all groups the user is will be filtered out so the user cannot match with their own groups
+                filterGroups.addAll(user.myGroups!!)
+                // all groups the user has already matched in will be filtered out
+                filterGroups.addAll(user.matchedGroups!!)
+                // all groups that are awaiting the voting process will be filtered out
+                filterGroups.addAll(user.pendingGroups!!)
+                // fetch all groups from the database filtering out the groups with
+                // names matching the unwanted group's name
+                db.collection("groups").whereNotIn("name", filterGroups)
+                    .get()
+                    .addOnSuccessListener {
+                        // convert the resulting groups into group object
+                        for (doc in it) {
+                            val group: Group = doc.toObject(Group::class.java)
+                            // add the group to the groups list
+                            groups.add(group)
+                        }
+                        // attach adapter and send groups and listener
+                        cardStackView.adapter = CardStackAdapter(groups, this)
+                    }
+            }
+
         // accept button accepts group when clicked
         val acceptBtn = binding.acceptBtn
         acceptBtn.setOnClickListener{
@@ -55,6 +100,8 @@ class MatchTabFragment : Fragment(), CardStackAdapter.CardStackAdapterListener {
                 .build()
             manager.setSwipeAnimationSetting(setting)
             cardStackView.swipe()
+            Log.v(TAG, binding.cardStackView.id.toString())
+//            currentUserDocRef.update("pendingGroups", FieldValue.arrayUnion(""))
         }
 
         // reject button rejects group when clicked
@@ -69,53 +116,6 @@ class MatchTabFragment : Fragment(), CardStackAdapter.CardStackAdapterListener {
             manager.setSwipeAnimationSetting(setting)
             cardStackView.swipe()
         }
-
-        // manager will define the layout for the card stack view
-        cardStackView.layoutManager = manager
-
-        /*
-        * fetch all possible groups to match based on logged in user
-        * and send to adapter which will display the groups in a recycler view
-         */
-        val groups = arrayListOf<Group>()
-        val db = Firebase.firestore
-        // get the authenticated logged in user
-        val currentUser = Firebase.auth.currentUser
-        // check if user exists / is still authenticated
-        if (currentUser != null) {
-            // fetches a user from firestore using the uid from the authenticated user
-            db.collection("users").document(currentUser.uid)
-                // reads the document reference
-                .get()
-                // if successful filter out certain groups for matching
-                .addOnSuccessListener { result ->
-                    // convert the fetched user into a User object
-                    val user: User = result.toObject(User::class.java)!!
-                    // filter groups will store all group to remove from the match group pool
-                    val filterGroups: ArrayList<String> = ArrayList()
-                    // all groups the user is will be filtered out so the user cannot match with their own groups
-                    filterGroups.addAll(user.myGroups!!)
-                    // all groups the user has already matched in will be filtered out
-                    filterGroups.addAll(user.matchedGroups!!)
-                    // all groups that are awaiting the voting process will be filtered out
-                    filterGroups.addAll(user.pendingGroups!!)
-                    // fetch all groups from the database filtering out the groups with
-                    // names matching the unwanted group's name
-                    db.collection("groups").whereNotIn("name", filterGroups)
-                        .get()
-                        .addOnSuccessListener {
-                            // convert the resulting groups into group object
-                            for (doc in it) {
-                                val group: Group = doc.toObject(Group::class.java)
-                                // add the group to the groups list
-                                groups.add(group)
-                            }
-                            // attach adapter and send groups and listener
-                            cardStackView.adapter = CardStackAdapter(groups, this)
-                        }
-                }
-        }
-
         // test groups without fetching for speed
 //        val g1 = Group("One Direction", arrayListOf(User("Harry", "Styles", "harrystyles@gmail.com")), arrayListOf("singing", "dancing", "partying", "soccer"), "one direction test description")
 //        val g2 = Group("The Beatles", arrayListOf(User("John", "Lennon", "johnlennon@gmail.com")), arrayListOf("cooking", "karaoke", "gaming", "movies"), "the beatles test description")
