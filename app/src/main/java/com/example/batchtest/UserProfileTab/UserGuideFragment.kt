6559b7a5e5e2ec522abf3c30b39e2758981039f5
Victoria Tran <mynamedummyy@gmail.com>
2023-03-29 13:59:50 -0700

    added user guide fragment and dialogs

--- app/src/main/java/com/example/batchtest/UserProfileTab/UserGuideFragment.kt
+++ app/src/main/java/com/example/batchtest/UserProfileTab/UserGuideFragment.kt
@@ -1,18 +1,16 @@
 package com.example.batchtest.UserProfileTab
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.batchtest.R
 import com.example.batchtest.databinding.FragmentAccountSettingBinding
 import com.example.batchtest.databinding.FragmentUserGuideBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +20,7 @@ private const val ARG_PARAM2 = "param2"
 class UserGuideFragment : Fragment() {
     private var _binding: FragmentUserGuideBinding? = null
     private val binding get() = _binding!!
+    private lateinit var dialog: Dialog
 
 
     override fun onCreateView(
@@ -29,7 +28,58 @@ class UserGuideFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentUserGuideBinding.inflate(layoutInflater, container, false)
+        dialog = Dialog(requireActivity())
+        val backButton = dialog.findViewById<Button>(R.id.btn_user_guide)
+        /*
+         * dialog for join a group
+         */
+        binding.joinAGroupLayout.setOnClickListener{
+            dialog.setContentView(R.layout.join_group_guide)
+            dialog.show()
+            backButton.setOnClickListener{
+                dialog.dismiss()
+            }
+        }
 
+        /*
+         * dialog for create a group
+         */
+
+        binding.createAGroupLayout.setOnClickListener{
+            dialog.setContentView(R.layout.create_group_guide)
+            dialog.show()
+            backButton.setOnClickListener{
+                dialog.dismiss()
+            }
+        }
+
+        /*
+         * dialog for set primary group
+         */
+        binding.setPrimaryLayout.setOnClickListener{
+            dialog.setContentView(R.layout.set_primary_guide)
+            dialog.show()
+            backButton.setOnClickListener{
+                dialog.dismiss()
+            }
+        }
+
+        /*
+         * dialog for 'batching'
+         */
+        binding.batchingLayout.setOnClickListener{
+            dialog.setContentView(R.layout.batching_guide)
+            dialog.show()
+            backButton.setOnClickListener{
+                dialog.dismiss()
+            }
+        }
+
+
+
+        binding.btnToUserProfileTab.setOnClickListener{
+            findNavController().navigate(R.id.action_userGuideFragment_to_userProfileTabFragment)
+        }
 
         return binding.root
     }
